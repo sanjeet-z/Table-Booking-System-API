f6de41891340ae9ea1087b7783aa4ae4f92b74a6
Sanjeet Kumar <106557362+sanjeet-z@users.noreply.github.com>
2024-08-23 02:28:27 +0530

    Update UserConstants.cs

--- Restaurant Table Booking/Shared/Constants/UserConstants.cs
+++ Restaurant Table Booking/Shared/Constants/UserConstants.cs
@@ -2,19 +2,19 @@
 {
     public class UserConstants
     {
-        public const string adminMail = "peeyush.s2g@gmail.com";
+        public const string adminMail = "sanjeet@gmail.com";
         public const string mailPassword = "fmbj bgjh gibb cxho";
 
         public const string mailBody = "<html>" +
                                          "<body>" +
                                          "<h1>Hello Sir!</h1>" +
-                                          "<h2>Your password has been changed. if You are not then recover your password and claim now!</h2>" +
+                                          "<h2>Your password has been changed. If this was not done by you then follow the steps to secure your account.</h2>" +
                                           "<br>" +
-                                          "<h3>Is this your mai id:</h3>" + "<h3>{UserEmail}</h3>" +
+                                          "<h3>Is this your mail id:</h3>" + "<h3>{UserEmail}</h3>" +
                                           "</body>" +
                                          "</html>";
 
-        public const string CouponMessage = "Coupon is InValid! or it is already used!";
+        public const string CouponMessage = "Coupon is Invalid or it has already been redeemed.";
 
         public const string DataAddedMessage = "Data added successfully!";
 
@@ -22,12 +22,12 @@
         public const string DataDeletedMessage = "Data Deleted successfully!";
         public const string DataUpdatedMessage = "Data updated successfully!";
         public const string ErrorMessage = "Something went wrong!";
-        public const string PasswordChanged = "Password Change successfully!";
+        public const string PasswordChanged = "Password Changed successfully!";
         public const string LogoutMessage = "Logout successfully!";
         public const string UnhandledError = "An unhandled error occurred";
-        public const string UnauthourizedMessage = "Please enter correct username or password";
+        public const string UnauthourizedMessage = "username or password incorrect!";
 
-        public const string EmailNotFound = "Please write correct email this email does not have account.";
+        public const string EmailNotFound = "Please enter correct Email. There is no account associated with this email.";
 
         public const string InvalidPassword = "Password does not match old password";
     }
